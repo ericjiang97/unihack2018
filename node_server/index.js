31ba5106a4de8aa6cce7714a2543aebbff63c41b
Ben435 <ben.clare@monash.edu>
2018-07-28 06:15:30 +1000

    Set priorities

--- node_server/index.js
+++ node_server/index.js
@@ -4,6 +4,8 @@ const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const Datastore = require("@google-cloud/datastore");
 const bodyParser = require("body-parser");
+const {google} = require('googleapis');
+
 
 const datastore = new Datastore({
   projectId: "calad-unihack"
@@ -84,6 +86,53 @@ app.get(
     res.redirect("/");
   }
 );
+
+app.put("/setPriority", (req, res) => {
+    let body = req.body;
+
+    let eventId = body['eventId'];
+    let prio = body['priority'];
+
+    let eventPriorityKey = datastore.key(["EventPriorities"]);
+    let data = {
+        eventId: eventId,
+        priority: prio,
+    };
+
+    datastore.save({
+        key: eventPriorityKey,
+        data: data
+    }).then(() => {
+        console.log(data);
+    }).catch((e) => {
+        console.log(e);
+    });
+
+    res.send(data);
+});
+
+function listEvents(auth) {
+    const calendar = google.calendar({version: 'v3', auth});
+    calendar.events.list({
+        calendarId: 'primary',
+        timeMin: (new Date()).toISOString(),
+        maxResults: 10,
+        singleEvents: true,
+        orderBy: 'startTime',
+    }, (err, res) => {
+        if (err) return console.log('The API returned an error: ' + err);
+        const events = res.data.items;
+        if (events.length) {
+            console.log('Upcoming 10 events:');
+            events.map((event, i) => {
+                const start = event.start.dateTime || event.start.date;
+                console.log(`${start} - ${event.summary}`);
+            });
+        } else {
+            console.log('No upcoming events found.');
+        }
+    });
+}
 
 app.listen(APPLICATION_PORT, () =>
   console.log(`Example app listening on port ${APPLICATION_PORT}!`)
