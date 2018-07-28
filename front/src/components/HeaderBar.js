3b14472e7a3c0c59e8e96f2746ee4195cb1a1f26
Nicholas Whittaker <nchlswhttkr@gmail.com>
2018-07-28 18:31:05 +1000

    added mock data

--- front/src/components/HeaderBar.js
+++ front/src/components/HeaderBar.js
@@ -1,20 +1,15 @@
 import React, { Component } from 'react'
 import { AppBar, Toolbar } from '@material-ui/core'
-import Logo from '../images/calad-icon-single.png'
-import { AccountCircle } from '@material-ui/icons'
+import logo from '../images/calad-icon-single.png'
+import Typography from '@material-ui/core/Typography'
 
 class HeaderBar extends Component {
   render() {
     return (
       <AppBar position="sticky">
-        <Toolbar style={{ padding: '10px' }}>
-          <img
-            src={Logo}
-            alt="Calad Logo"
-            style={{ width: '100px', height: '100px' }}
-          />
-          <h1 style={{ marginTop: '50px' }}>CALAD</h1>
-          <AccountCircle style={{ width: '50px', height: '50px' }} />
+        <Toolbar>
+          <img src={logo} style={{ height: 80 }} />
+          <Typography variant="title">CALAD</Typography>
         </Toolbar>
       </AppBar>
     )
