0ccaa99b22a374c4a9b9d1327fb3793d88af80c6
Ben435 <ben.clare@monash.edu>
2018-07-29 11:38:24 +1000

    Font changes v2

--- front/src/pages/OnboardingSecondPage.js
+++ front/src/pages/OnboardingSecondPage.js
@@ -55,137 +55,144 @@ class OnboardingFirstPage extends Component {
             </Typography>
           </header>
         </Fade>
-        <Fade in={true} timeout={1500}>
-          <Typography
-            variant="subheading"
-            gutterBottom
-            style={{ color: 'white' }}
-          >
-              Let us help you make the best of your events on your calendar.
-              Whether it's conferences, meetings with execs, etc.
-
-              How do the following statements describe how <b>you</b> manage your timetable?
-          </Typography>
-        </Fade>
+        <div>
+          <Fade in={true} timeout={1500}>
+            <Typography
+              variant="subheading"
+              gutterBottom
+              style={{ color: 'white', fontSize: "110%"}}
+            >
+              <p>
+                Let us help you make the best of your events on your calendar.
+                Whether it's conferences, meetings with execs, etc.
+              </p>
+              <p>
+                How do the following statements describe how <b>you</b> manage your timetable?
+              </p>
+            </Typography>
+          </Fade>
+        </div>
         <Divider />
-        <Fade in={true} timeout={1750}>
-            <FormControl required={Object.values(this.state).filter(i => i != null).length === Object.values(this.state).length}>
-                <RadioPsychTest name={"I'm most effective when I get a break between consecutive meetings, to recharge and prepare for the next meeting"}
-                                value={this.state.positive_consec_meetings}
-                                onChange={(val) => this.setState({positive_consec_meetings: val})}/>
+        <div style={{top: "4px", position: "relative"}}>
+          <Fade in={true} timeout={1750}>
+              <FormControl required={Object.values(this.state).filter(i => i != null).length === Object.values(this.state).length}>
+                  <RadioPsychTest name={"I'm most effective when I get a break between consecutive meetings, to recharge and prepare for the next meeting"}
+                                  value={this.state.positive_consec_meetings}
+                                  onChange={(val) => this.setState({positive_consec_meetings: val})}/>
 
-                <RadioPsychTest name={"I'm OK with a late lunch"}
-                                value={this.state.positive_late_lunch}
-                                onChange={val => this.setState({positive_late_lunch: val})}/>
+                  <RadioPsychTest name={"I'm OK with a late lunch"}
+                                  value={this.state.positive_late_lunch}
+                                  onChange={val => this.setState({positive_late_lunch: val})}/>
 
-                <RadioPsychTest name={"I'm OK with sometimes leaving meetings early"}
-                                value={this.state.positive_leave_early}
-                                onChange={val => this.setState({positive_leave_early: val})}/>
+                  <RadioPsychTest name={"I'm OK with sometimes leaving meetings early"}
+                                  value={this.state.positive_leave_early}
+                                  onChange={val => this.setState({positive_leave_early: val})}/>
 
-                <RadioPsychTest name={"I have rarely do working lunches"}
-                                value={this.state.negative_no_lunch}
-                                onChange={val => this.setState({negative_no_lunch: val})}/>
+                  <RadioPsychTest name={"I rarely take working lunches"}
+                                  value={this.state.negative_no_lunch}
+                                  onChange={val => this.setState({negative_no_lunch: val})}/>
 
-                <RadioPsychTest name={"I don't mind missing the start of less important meetings"}
-                                value={this.state.negative_late_arrival}
-                                onChange={val => this.setState({negative_late_arrival: val})}/>
+                  <RadioPsychTest name={"I don't mind missing the start of some meetings"}
+                                  value={this.state.negative_late_arrival}
+                                  onChange={val => this.setState({negative_late_arrival: val})}/>
 
-                <RadioPsychTest name={"I hate being late to meetings"}
-                                value={this.state.negative_late_meeting}
-                                onChange={val => this.setState({negative_late_meeting: val})}/>
+                  <RadioPsychTest name={"I hate being late to meetings"}
+                                  value={this.state.negative_late_meeting}
+                                  onChange={val => this.setState({negative_late_meeting: val})}/>
 
-                <RadioPsychTest name={"I believe the most important part of the meeting is near the end"}
-                                value={this.state.negative_leave_early}
-                                onChange={val => this.setState({negative_leave_early: val})}/>
+                  <RadioPsychTest name={"I believe the most important part of the meeting is near the end"}
+                                  value={this.state.negative_leave_early}
+                                  onChange={val => this.setState({negative_leave_early: val})}/>
 
-                <RadioPsychTest name={"I sometimes have early lunches"}
-                                value={this.state.positive_early_lunch}
-                                onChange={val => this.setState({positive_early_lunch: val})}/>
+                  <RadioPsychTest name={"I sometimes have early lunches"}
+                                  value={this.state.positive_early_lunch}
+                                  onChange={val => this.setState({positive_early_lunch: val})}/>
 
-                <RadioPsychTest name={"I require a break while I eat my lunch"}
-                                value={this.state.negative_working_lunch}
-                                onChange={val => this.setState({negative_working_lunch: val})}/>
+                  <RadioPsychTest name={"I require a break while I eat my lunch"}
+                                  value={this.state.negative_working_lunch}
+                                  onChange={val => this.setState({negative_working_lunch: val})}/>
 
-                <RadioPsychTest name={"I like to cluster all my meetings one after another"}
-                                value={this.state.positive_low_context_switch}
-                                onChange={val => this.setState({positive_low_context_switch: val})}/>
-            </FormControl>
-          {/*<div>*/}
-            {/*<Typography variant="title" gutterBottom style={{ color: 'white' }}>*/}
-              {/*Consecutive events*/}
-            {/*</Typography>*/}
-            {/*<Typography*/}
-              {/*variant="subheading"*/}
-              {/*gutterBottom*/}
-              {/*style={{ color: 'white' }}*/}
-            {/*>*/}
-              {/*When you have Consecutive (or back-to-back events), would you*/}
-              {/*prefer:*/}
-            {/*</Typography>*/}
-            {/*<ul>*/}
-              {/*<li>Leave previous event early</li>*/}
-              {/*<li>Arrive next event late</li>*/}
-            {/*</ul>*/}
-
-            {/*<FormControl>*/}
-              {/*<InputLabel htmlFor="name-readonly">Name</InputLabel>*/}
-              {/*<Select*/}
-                {/*value={this.state.consecutiveMeetings}*/}
-                {/*onChange={event => {*/}
-                  {/*this.setState({*/}
-                    {/*consecutiveMeetings: event.target.value,*/}
-                  {/*})*/}
-                {/*}}*/}
-                {/*input={*/}
-                  {/*<Input*/}
-                    {/*name="Select a choice"*/}
-                    {/*id="name-readonly"*/}
-                    {/*style={{ color: 'white' }}*/}
-                  {/*/>*/}
-                {/*}*/}
+                  <RadioPsychTest name={"I like to cluster all my meetings one after another"}
+                                  value={this.state.positive_low_context_switch}
+                                  onChange={val => this.setState({positive_low_context_switch: val})}/>
+              </FormControl>
+            {/*<div>*/}
+              {/*<Typography variant="title" gutterBottom style={{ color: 'white' }}>*/}
+                {/*Consecutive events*/}
+              {/*</Typography>*/}
+              {/*<Typography*/}
+                {/*variant="subheading"*/}
+                {/*gutterBottom*/}
+                {/*style={{ color: 'white' }}*/}
               {/*>*/}
-                {/*<MenuItem value="before">Leave event early</MenuItem>*/}
-                {/*<MenuItem value="after">Arrive next event late</MenuItem>*/}
-              {/*</Select>*/}
-            {/*</FormControl>*/}
-            {/*<Divider style={{ marginBottom: 10 }} />*/}
-            {/*<Typography variant="title" gutterBottom style={{ color: 'white' }}>*/}
-              {/*Leave half way for longer events*/}
-            {/*</Typography>*/}
-            {/*<Typography*/}
-              {/*variant="subheading"*/}
-              {/*gutterBottom*/}
-              {/*style={{ color: 'white' }}*/}
-            {/*>*/}
-              {/*When you have an event for the whole day, and another important*/}
-              {/*event in the middle of the day, would you like to accept both*/}
-              {/*events:*/}
-            {/*</Typography>*/}
-            {/*<FormControl>*/}
-              {/*<InputLabel htmlFor="acceptBothEvents">Select one:</InputLabel>*/}
-              {/*<Select*/}
-                {/*value={this.state.acceptBothEvents}*/}
-                {/*onChange={event => {*/}
-                  {/*this.setState({*/}
-                    {/*acceptBothEvents: event.target.value,*/}
-                  {/*})*/}
-                {/*}}*/}
-                {/*input={*/}
-                  {/*<Input*/}
-                    {/*name="Select a choice"*/}
-                    {/*id="acceptBothEvents"*/}
-                    {/*style={{ color: 'white' }}*/}
-                  {/*/>*/}
-                {/*}*/}
-              {/*>*/}
-                {/*<MenuItem value="both">Accept both events</MenuItem>*/}
-                {/*<MenuItem value="reject">Reject both events</MenuItem>*/}
-                {/*<MenuItem value="decide">Prompt me to decide</MenuItem>*/}
-              {/*</Select>*/}
+                {/*When you have Consecutive (or back-to-back events), would you*/}
+                {/*prefer:*/}
+              {/*</Typography>*/}
+              {/*<ul>*/}
+                {/*<li>Leave previous event early</li>*/}
+                {/*<li>Arrive next event late</li>*/}
+              {/*</ul>*/}
 
-            {/*</FormControl>*/}
-          {/*</div>*/}
-        </Fade>
+              {/*<FormControl>*/}
+                {/*<InputLabel htmlFor="name-readonly">Name</InputLabel>*/}
+                {/*<Select*/}
+                  {/*value={this.state.consecutiveMeetings}*/}
+                  {/*onChange={event => {*/}
+                    {/*this.setState({*/}
+                      {/*consecutiveMeetings: event.target.value,*/}
+                    {/*})*/}
+                  {/*}}*/}
+                  {/*input={*/}
+                    {/*<Input*/}
+                      {/*name="Select a choice"*/}
+                      {/*id="name-readonly"*/}
+                      {/*style={{ color: 'white' }}*/}
+                    {/*/>*/}
+                  {/*}*/}
+                {/*>*/}
+                  {/*<MenuItem value="before">Leave event early</MenuItem>*/}
+                  {/*<MenuItem value="after">Arrive next event late</MenuItem>*/}
+                {/*</Select>*/}
+              {/*</FormControl>*/}
+              {/*<Divider style={{ marginBottom: 10 }} />*/}
+              {/*<Typography variant="title" gutterBottom style={{ color: 'white' }}>*/}
+                {/*Leave half way for longer events*/}
+              {/*</Typography>*/}
+              {/*<Typography*/}
+                {/*variant="subheading"*/}
+                {/*gutterBottom*/}
+                {/*style={{ color: 'white' }}*/}
+              {/*>*/}
+                {/*When you have an event for the whole day, and another important*/}
+                {/*event in the middle of the day, would you like to accept both*/}
+                {/*events:*/}
+              {/*</Typography>*/}
+              {/*<FormControl>*/}
+                {/*<InputLabel htmlFor="acceptBothEvents">Select one:</InputLabel>*/}
+                {/*<Select*/}
+                  {/*value={this.state.acceptBothEvents}*/}
+                  {/*onChange={event => {*/}
+                    {/*this.setState({*/}
+                      {/*acceptBothEvents: event.target.value,*/}
+                    {/*})*/}
+                  {/*}}*/}
+                  {/*input={*/}
+                    {/*<Input*/}
+                      {/*name="Select a choice"*/}
+                      {/*id="acceptBothEvents"*/}
+                      {/*style={{ color: 'white' }}*/}
+                    {/*/>*/}
+                  {/*}*/}
+                {/*>*/}
+                  {/*<MenuItem value="both">Accept both events</MenuItem>*/}
+                  {/*<MenuItem value="reject">Reject both events</MenuItem>*/}
+                  {/*<MenuItem value="decide">Prompt me to decide</MenuItem>*/}
+                {/*</Select>*/}
+
+              {/*</FormControl>*/}
+            {/*</div>*/}
+          </Fade>
+        </div>
         <Fade in={true} timeout={2200}>
           <Button variant="raised" color="primary" href="./final">
             Submit
