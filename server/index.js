f6543b4e322a14bea91905323988acc4a7ab5dea
Eric Jiang <ericjiang97@gmail.com>
2018-07-29 11:56:29 +1000

    code

--- server/index.js
+++ server/index.js
@@ -132,4 +132,9 @@ app.get("/api/calendar", userLogged, (req, res) => {
   );
 });
 
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
